fb771513415325ba4541af4ca84daab269f4fc51
Leslye Pérez <leslye@concepthaus.mx>
2021-11-11 11:44:29 -0600

    se sube seccion de pagar ahora

--- db/db_kiperweb.sql
+++ db/db_kiperweb.sql
@@ -2,7 +2,8 @@
 SQLyog Ultimate v11.11 (64 bit)
 MySQL - 5.5.5-10.4.21-MariaDB : Database - db_kip3r_web
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -12,9 +13,9 @@ MySQL - 5.5.5-10.4.21-MariaDB : Database - db_kip3r_web
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_kip3r_web` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`kip3r_web` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-USE `db_kip3r_web`;
+USE `kip3r_web`;
 
 /*Table structure for table `backend_access_log` */
 
